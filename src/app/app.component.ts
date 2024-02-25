3414c8481fa0c1f4cd9c66122c54f2279b260209
artur-helta <artur.helta@onet.pl>
2024-02-25 14:48:18 +0100

    create project template and and select and show photo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { DomSanitizer } from '@angular/platform-browser';
+import { FileHandler } from './app.model';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +10,65 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'photo-form';
+
+  productForm!: FormGroup;
+  @ViewChild('fileInput') fileInput!: ElementRef;
+  selectedFiles: FileHandler[] = [];
+
+  constructor(
+    private formBuilder: FormBuilder,
+    private sanitazier: DomSanitizer
+  ){}
+  
+  ngOnInit() {
+    this.productForm = this.formBuilder.group({
+      productName: ['', Validators.required],
+      productPrice: ['', Validators.required],
+      productDescription: ['', Validators.required],
+      productImages: ['', Validators.required] 
+    });
+  }
+
+  onSubmit(){}
+
+  onFileSelect(event: Event) {
+    const input = event.target as HTMLInputElement;
+    if (!input.files || input.files.length === 0) {
+      return;
+    }
+  
+    // Pętla przez wszystkie nowo wybrane pliki
+    for (let i = 0; i < input.files.length; i++) {
+      if (this.selectedFiles.length >= 4) {
+        // Przekroczenie limitu plików
+        alert("Można przesłać maksymalnie 4 zdjęcia.");
+        break;
+      }
+  
+      const file = input.files[i];
+  
+      // Sprawdź typ pliku (opcjonalnie można dodać dodatkowe walidacje, np. rozmiar pliku)
+      if (file.type.match(/image\/*/) && !this.selectedFiles.some(f => f.file.name === file.name)) {
+        const fileHandler: FileHandler = {
+          file: file,
+          url: this.sanitazier.bypassSecurityTrustUrl(
+            URL.createObjectURL(file)
+          )
+        }
+        this.selectedFiles.push(fileHandler);
+     
+      } else {
+        alert("Dozwolone są tylko pliki obrazów.");
+      }
+    }
+  }
+
+  onUploadButtonClick() {
+    this.fileInput.nativeElement.click();
+  }
+  
+  removeFile(index: number): void {
+    this.selectedFiles.splice(index, 1);
+  }
 }
+
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatInputModule} from '@angular/material/input';
+import { ReactiveFormsModule } from '@angular/forms';
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
 
 @NgModule({
   declarations: [
@@ -10,7 +15,12 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatInputModule,
+    ReactiveFormsModule,
+    MatButtonModule,
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
